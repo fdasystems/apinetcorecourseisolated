5388db375ffbb31ed54054a77e2fbcaf43ee0678
Favio Andrada <desa2.it@assurant.com>
2020-11-22 01:36:09 -0300

    Create Linked Entities

--- ApiCourseIsolated/Data/ApplicationDbContext.cs
+++ ApiCourseIsolated/Data/ApplicationDbContext.cs
@@ -10,9 +10,26 @@ namespace ApiCourseIsolated.Data
 {
     public class ApplicationDbContext : IdentityDbContext<CustomUser>
     {
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        /*
+        public ApplicationDbContext()
+        {
+        }*/
+
+
+        public DbSet<MainCourse> MainCourse { get; set; }      
+        public DbSet<DetailCourse> DetailCourse { get; set; }
+        
+        /*  Ver si hace falta 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+        */
+       
     }
 }
---
+++ ApiCourseIsolated/Entities/DetailCourse.cs
@@ -0,0 +1,14 @@
+﻿using ApiCourseIsolated.Entities.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiCourseIsolated.Entities
+{
+    public class DetailCourse : EntityBase
+    {
+        [Required]
+        public string UrlLink { get; set; }
+        public int Order { get; set; }
+        public int MainCourseId { get; set; }
+        public MainCourse MainCourse { get; set; }
+    }
+}
