f1961cb7941d64a3f72c737999eacf13ec3971ba
Favio Andrada <desa2.it@assurant.com>
2020-11-22 23:05:16 -0300

    addExtraData

--- ApiCourseIsolated/Migrations/20201123020214_addExtraData.Designer.cs
+++ ApiCourseIsolated/Migrations/20201123020214_addExtraData.Designer.cs
@@ -4,14 +4,16 @@ using ApiCourseIsolated.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ApiCourseIsolated.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201123020214_addExtraData")]
+    partial class addExtraData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
